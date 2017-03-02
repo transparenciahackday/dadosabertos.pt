50233d2a9da50f7c9b939c023b6edfae7a311eaa
Ricardo Lafuente <r@manufacturaindependente.org>
2017-03-02 22:09:08 +0000

    Adicionar função para obter HTML a partir dos ficheiros md

--- layout/_functions.php
+++ layout/_functions.php
@@ -1,10 +1,18 @@
 <?php
-  
+
   // Menu active page
   function active_page($current_page, $page) {
     if ($current_page == $page) {
       echo " class='active'";
     }
   }
-  
+
+  require_once 'Michelf/MarkdownExtra.inc.php';
+  function html_from_markdown($filename) {
+    $page_contents_markdown = file_get_contents('pages/' . $filename);
+    $md = new \Michelf\MarkdownExtra();
+    $page_contents_html = $md->defaultTransform($page_contents_markdown);
+    return $page_contents_html;
+  }
+
 ?>
