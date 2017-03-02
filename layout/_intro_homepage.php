94a720c0b75502f2f8a70035904cfdf97b2990a1
Ricardo Lafuente <r@manufacturaindependente.org>
2017-03-02 22:09:28 +0000

    Teste da função com uma das colunas da intro

--- layout/_intro_homepage.php
+++ layout/_intro_homepage.php
@@ -3,8 +3,7 @@
         <div class="container">
           <div class="row">
             <div class="col-sm-6 col-md-6">
-              <p>Os dados abertos (ou open data em inglês) são dados que podem ser livremente usados, reutilizados e redistribuídos por qualquer pessoa - sujeitos, no máximo, à exigência de atribuição da fonte e partilha segundo os mesmos termos.</p>
-              <p>Quando falamos de dados, estamos a referir-nos a conjuntos de informação estruturada; na gíria técnica, referimo-nos a datasets ou bases de dados. São bons exemplos os censos populacionais, registos meteorológicos, registos de empresas ou transcrições parlamentares.</p>
+              <?php echo html_from_markdown('intro-col-1.md'); ?>
             </div>
             <div class="col-sm-6 col-md-6">
               <p>Os Estados, Parlamentos e inúmeras instituições públicas produzem conjuntos de dados de enorme valor. Na maior parte dos casos, tratam-se de informação pública destinada a ser disseminada.</p>
