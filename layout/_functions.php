d76d60d0884f3ac1e282614903130d2d1e056206
Ricardo Lafuente <r@manufacturaindependente.org>
2017-03-02 22:10:38 +0000

    "/pages/" -> "/content/"

--- layout/_functions.php
+++ layout/_functions.php
@@ -9,7 +9,7 @@
 
   require_once 'Michelf/MarkdownExtra.inc.php';
   function html_from_markdown($filename) {
-    $page_contents_markdown = file_get_contents('pages/' . $filename);
+    $page_contents_markdown = file_get_contents('content/' . $filename);
     $md = new \Michelf\MarkdownExtra();
     $page_contents_html = $md->defaultTransform($page_contents_markdown);
     return $page_contents_html;
