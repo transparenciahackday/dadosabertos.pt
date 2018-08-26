c0a92d761df18b4dd2d1766e2863be857459ec6c
Alexandre Pereira <alexandremmartinsp@gmail.com>
2018-08-26 11:46:00 +0100

    favicon folder added + new favicon created + little fix in <hea>

--- layout/_head.php
+++ layout/_head.php
@@ -7,8 +7,16 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,  user-scalable=no" />
 
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="favicon.png">
+	<link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="favicon/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-touch-icon-152x152.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon-180x180.png" />
 
     <!-- Open Graph -->
     <meta property="og:title" content="<?php echo $page_title ?>" />
@@ -16,7 +24,7 @@
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://dadosabertos.pt" />
     <meta property="og:image" content="https://dadosabertos.pt/img/og_share.png" />
-    <meta property="og:locale"" content="pt_PT" />
+    <meta property="og:locale" content="pt_PT" />
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Titillium+Web:700,900" rel="stylesheet">
